4136baf981e4976410cd8636bdbc1adf3e4ebaca
thecknt <barretocristianr@gmail.com>
2025-07-11 19:39:56 -0300

    esctructure version of final

---
+++ FinalTAP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cristian_Barreto
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
